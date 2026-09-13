45c1a5396c053b6fce7428758aadd5a28ed7cef0
carolina <carolina@MONNIE>
2026-09-13 13:56:12 -0400

    se agrego delete y edit para el funcionamiento del apartado opciones del index

--- GestionLibrosBlazor/GestionLibrosBlazor/Program.cs
+++ GestionLibrosBlazor/GestionLibrosBlazor/Program.cs
@@ -31,7 +31,7 @@ if (!app.Environment.IsDevelopment())
 }
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapStaticAssets();
