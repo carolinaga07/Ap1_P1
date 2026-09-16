fb937b022f065cbe0a5418b74c6bd7fbe53b268e
carolina <carolina@MONNIE>
2026-09-15 23:34:23 -0400

    inicio de la tarea 2 con la creacion de la clase estudiantes

---
+++ GestionLibrosBlazor/GestionLibrosBlazor/Models/Estudiantes.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
+
+namespace GestionLibrosBlazor.Models
+{
+    public class Estudiantes
+    {
+        [Key]
+        public int EstudianteId { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        public string Nombres { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "La direccion es requerido")]
+        public string Direccion { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El email es requerido")]
+
+        public string Email { get; set; } = string.Empty;
+
+        public DateFormat FechaNacimiento { get; set; }
+
+
+    }
+}
