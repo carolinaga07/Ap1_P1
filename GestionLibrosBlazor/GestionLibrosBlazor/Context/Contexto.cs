099f837899d6aa279f56ce33325f6c28396b3db7
Carolina Garcia Almanzar <carolina_garcia1@ucne.edu.do>
2026-09-21 21:27:53 -0400

    Revert "Revert "Registro estudiantes blazor""

--- GestionLibrosBlazor/GestionLibrosBlazor/Context/Contexto.cs
+++ GestionLibrosBlazor/GestionLibrosBlazor/Context/Contexto.cs
@@ -10,6 +10,7 @@ namespace GestionLibrosBlazor.Context
 
         }
         public DbSet<Libros> Libros { get; set; }
+        public DbSet <Estudiantes > Estudiantes { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
--- GestionLibrosBlazor/GestionLibrosBlazor/Program.cs
+++ GestionLibrosBlazor/GestionLibrosBlazor/Program.cs
@@ -13,11 +13,12 @@ builder.Services.AddRazorComponents()
 
 //contexto
 var ConStr = builder.Configuration.GetConnectionString("ConStr");
-builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlite(ConStr));
+builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlServer(ConStr));
 
 //servicio
 
 builder.Services.AddScoped<LibrosService>();
+builder.Services.AddScoped<EstudiantesService>();
 
 builder.Services.AddBlazorBootstrap();
 
